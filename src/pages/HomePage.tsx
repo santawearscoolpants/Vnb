9057d34d0b5bb0dd68ae9e94de5b1f35bca23ec0
santawearscoolpants <elijahnansuuri46@gmail.com>
2026-03-19 01:18:22 +0000

    Update index.html to reference new JavaScript file; remove obsolete index-C0Yg5ov_.js and index.es-cd6o4jiZ.js files. Remove BrandStory component from HomePage for a streamlined layout.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,18 +3,16 @@ import { ProductCategories } from '../components/ProductCategories';
 import { SandalsSection } from '../components/SandalsSection';
 import { FeaturedCollection } from '../components/FeaturedCollection';
 import { CollectionsGrid } from '../components/CollectionsGrid';
-import { BrandStory } from '../components/BrandStory';
 import { Newsletter } from '../components/Newsletter';
 
 export function HomePage() {
   return (
     <>
       <Hero />
-      <ProductCategories />
       <SandalsSection />
       <CollectionsGrid />
       <FeaturedCollection />
-      <BrandStory />
+      <ProductCategories />
       <Newsletter />
     </>
   );
